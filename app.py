3f727536fc5b71c1dcf5c7597e8fbb22a712943a
EC2 Default User <ec2-user@ip-172-31-0-68.ec2.internal>
2026-02-04 11:45:20 +0000

    Updated production app

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-Hello production
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Production DevOps Pipeline Running 🚀"
+
+@app.route('/health')
+def health():
+    return "App is healthy ✅"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
