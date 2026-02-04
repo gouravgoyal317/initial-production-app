0d98526ffc9d080840978ab947710b8769222eb1
EC2 Default User <ec2-user@ip-172-31-0-68.ec2.internal>
2026-02-04 11:17:27 +0000

    test auto deploy

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Production DevOps Pipeline Running 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+Hello production
 
